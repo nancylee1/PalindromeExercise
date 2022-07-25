30909daa73411913b6eb902216da49a0dbf10a6e
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-07-25 16:56:13 -0400

    init commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            if (word == null) 
+            { 
+                return false;
+            }   
+            return word.Replace(" ", "").ToLower().SequenceEqual(word.Replace(" ", "").ToLower().Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("dad", true)]
+        [InlineData("Hannah", true)]  // using Linq's .ToLower()
+        [InlineData("levels", false)]
+        [InlineData(null, false)]
+        [InlineData("", true)]
+        [InlineData("Was it a car or a cat I saw", true)]  // using Linq's .Replace(" ", "")
+        [InlineData("never odd or even", true)] // using Linq's .Replace(" ", "") to remove the spaces, then ending with .Reverse()) to check for Palindrome!
 
+        public void PalindromeTestTakesInString_OutputsTrueOrFalseIfPalindrome(string word, bool expected)
+        {
+            //Arrange - create a new wordsmith object
+            var wordSmith = new WordSmith(); 
+
+            //Act
+            var answer = wordSmith.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, answer);
         }
     }
 }
